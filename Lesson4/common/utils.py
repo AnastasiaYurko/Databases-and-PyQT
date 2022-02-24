baa4c36acd3ec4d63085a18b9c8244725ae888e4
Anastasia Yurko <anastasijajurko@mail.ru>
2022-02-24 13:39:01 +0300

    HW4

---
+++ Lesson4/common/utils.py
@@ -0,0 +1,29 @@
+from Lesson4.common.variables import *
+from Lesson4.errors import IncorrectDataRecivedError, NonDictInputError
+import json
+import sys
+from Lesson4.decos import log
+sys.path.append('../')
+
+
+@log
+def get_message(client):
+    encoded_response = client.recv(MAX_PACKAGE_LENGTH)
+    if isinstance(encoded_response, bytes):
+        json_response = encoded_response.decode(ENCODING)
+        response = json.loads(json_response)
+        if isinstance(response, dict):
+            return response
+        else:
+            raise IncorrectDataRecivedError
+    else:
+        raise IncorrectDataRecivedError
+
+
+@log
+def send_message(sock, message):
+    if not isinstance(message, dict):
+        raise NonDictInputError
+    js_message = json.dumps(message)
+    encoded_message = js_message.encode(ENCODING)
+    sock.send(encoded_message)
