90daba75c7f4a520b8b45af87a62b54e29764e78
Anastasia Yurko <anastasijajurko@mail.ru>
2022-03-02 12:49:42 +0300

    HW5

---
+++ Lesson5/common/utils.py
@@ -0,0 +1,23 @@
+import json
+import sys
+from Lesson5.common.decos import log
+from Lesson5.common.variables import *
+sys.path.append('../')
+
+
+@log
+def get_message(client):
+    encoded_response = client.recv(MAX_PACKAGE_LENGTH)
+    json_response = encoded_response.decode(ENCODING)
+    response = json.loads(json_response)
+    if isinstance(response, dict):
+        return response
+    else:
+        raise TypeError
+
+
+@log
+def send_message(sock, message):
+    js_message = json.dumps(message)
+    encoded_message = js_message.encode(ENCODING)
+    sock.send(encoded_message)
